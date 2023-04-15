5729f806e7831fb58b9ead0b0a67a07335df57e1
Mariano <mariano_tangari@hotamil.com>
2023-04-15 00:40:32 -0300

    Quickselect compacted.

--- search/quickselect.c
+++ search/quickselect.c
@@ -3,9 +3,7 @@
 int main() {
 
 	int arr[10] = {20, 15, 25, 40, 35, 45, 65, 70, 75, 80}; 
-	
 	int size = (int) sizeof(arr) / sizeof(arr[0]); 
-
 	int second_min = quickselect(arr, 0, size - 1, 4);  
 
 	printf("%d", second_min);
@@ -16,17 +14,16 @@ int quickselect(int* arr, int l, int h, int k) {
 	if(l == h) {
 		return arr[h]; 
 	}
-
+	
 	int p = partition(arr, l, h);
-
+	
 	if(k < p) {
 		return quickselect(arr, l, p - 1, k); 
 	}
-
 	if(k > p) {
 		return quickselect(arr, p + 1, h, k); 
 	}
-
+	
 	return arr[p]; 
 }
 
@@ -42,13 +39,12 @@ int partition(int* arr, int p, int pivot) {
             i++;
         }
     }
-        
     swap(i, pivot, arr);
     return i; 
 }
 
 void swap(int i, int j, int* arr){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    int temp = arr[i];
+    arr[i] = arr[j];
+	arr[j] = temp;
 }
